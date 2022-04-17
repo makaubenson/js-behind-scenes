f1078f658aa8f4600119ef4192fbddfaa8446187
Benson <bensonmakau2000@gmail.com>
2022-04-17 14:08:50 +0300

    function scope

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+// function to calculate age
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  //Function to print age
+  function printAge() {
+    const output = `${firstName},Your are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Benson';
+calcAge(1991);
