766f49f1fab2a54991eb5c9498f4a283243bc93f
Benson <bensonmakau2000@gmail.com>
2022-04-17 14:15:15 +0300

    block scoping

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `${firstName},Your are ${age}, born in ${birthYear}`;
     console.log(output);
+    //defining a block scope
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true; //function scoped
+      const str = `Oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str);// it will cause an error
+    console.log(millenial); // possible to access
   }
   printAge();
   return age;
